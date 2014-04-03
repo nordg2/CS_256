c55f9abcbbbca751f8ea31d971267df1e62fa6d3
nordg2 <bj_nordgren@hotmail.com>
2014-04-03 08:08:28 -0600

    some changes for css and getting the video page running

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <title>Video Page</title>
     </head>
-    <body>
-        <div>hello world</div>
+    <body class="background-grey">
+        <div id="header" class="background-grey"></div>
+        <div id="main_panel" class="background-grey">
+            <div id="left-panel" class="background-grey">
+                <iframe id="ytplayer" type="text/html" width="640px" height="390px" src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"></iframe>
+            </div>
+            <div id="right-panel" class="background-grey">
+                <div style="margin-left: 1em; margin-right: 1.35em;"><input id="search" class="search"/></div>
+                <div class="question shadow">
+                    <p>this is my sincere question?</p>
+                </div>
+                <div class="question shadow">
+                    <p>this is another question?</p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
