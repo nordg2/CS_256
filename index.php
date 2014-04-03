23a813c763cad798350745e20dc46d46acec3b65
nordg2 <bj_nordgren@hotmail.com>
2014-04-03 09:00:08 -0600

    more changes

--- index.php
+++ index.php
@@ -6,19 +6,26 @@
         <title>Video Page</title>
     </head>
     <body class="background-grey">
-        <div id="header" class="background-grey"></div>
+        
         <div id="main_panel" class="background-grey">
+            
             <div id="left-panel" class="background-grey">
+                <div class="header"><img id="home" src="home.png"/><span id="title">The Molecular Structure of Sugar</span></div>
                 <iframe id="ytplayer" type="text/html" width="640px" height="390px" src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"></iframe>
             </div>
             <div id="right-panel" class="background-grey">
+                <div class="header"><a id="signout" href="#">Sign out</a></div>
+                <!-- search bar -->
                 <div style="margin-left: 1em; margin-right: 1.35em;"><input id="search" class="search"/></div>
+                <!-- questions -->
                 <div class="question shadow">
-                    <p>this is my sincere question?</p>
+                    <p>this is my sincere question? kjsljksfdjk lljk sfjk ljkl jkl sdj klsdfjk lsdfj kl sjfkl jklsfdjkl sdfjk lsdfjkl sdf ljkdsfjklfsdjklsfdjkl sfdjkl dfsj kljksdfl ljk sfd</p>
+                    
                 </div>
                 <div class="question shadow">
-                    <p>this is another question?</p>
+                    <p>this is my sincere question? kjsljksfdjk lljk sfjk ljkl jkl sdj klsdfjk lsdfj kl sjfkl jklsfdjkl sdfjk lsdfjkl sdf ljkdsfjklfsdjklsfdjkl sfdjkl dfsj kljksdfl ljk sfd</p>
                 </div>
+                <button id="submit">Submit</button>
             </div>
         </div>
     </body>
